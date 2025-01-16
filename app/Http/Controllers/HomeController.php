bc0687a90007b61eefbbbffc8f043aef7ff1ffd3
David Avila <david.av.del.22@gmail.com>
2025-01-16 16:21:43 -0600

    Conexión con  Base de Datos
    Se creó la conexión con la base de datos, así como el ajuste a algunos métodos.

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Exports\ReporteExport;
+use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
+
+class HomeController extends Controller
+{
+    // Métodos para ingresar los rangos    
+    public function ingresarRangos () {
+        $actualYear = date('Y');
+        $months = [
+            0 => 'Enero',
+            1 => 'Febrero',
+            2 => 'Marzo',
+            3 => 'Abril',
+            4 => 'Mayo',
+            5 => 'Junio',
+            6 => 'Julio',
+            7 => 'Agosto',
+            8 => 'Septiembre',
+            9 => 'Octubre',
+            10 => 'Noviembre',
+            11 => 'Diciembre'
+        ];
+        $years = [];
+        for ($i = 2016; $i <= $actualYear; $i++) {
+            array_push($years, $i);
+        }
+        return view('rangos', compact(['months', 'years']));
+    }
+
+    public function procesarRangos (Request $request) {
+        $validated = $request->validate([
+            'reporte_anio' => 'required|numeric',
+            'mes_inicial' => 'required|integer|min:0|max:12',
+            'mes_final' => 'required|integer|min:0|max:12|gte:mes_inicial',
+        ]);        
+
+        $request->session()->put('rangos', $validated);
+        // $request->session()->put('datosPrevios', $request->only(['reporte_anio', 'mes_inicial', 'mes_final'])); only obtiene solo datos del $request, pero es mejor y más limpio validarlos antes
+        
+        return redirect()->route('home.ingresarReporte');
+    }
+
+    public function ingresarReporte () {
+        $reportes = [
+            1 => 'Principales Delitos Comparativo',
+            2 => 'Principales Delitos por Mes',
+            3 => 'Delitos de Alto Impacto',
+            4 => 'Secuestros',
+            5 => 'Extorsiones',
+            6 => 'Homicidios',
+            7 => 'Privación de la Libertad',
+            8 => 'Lesiones',
+            9 => 'Robo por Modalidad',
+            10 => 'Robo por Modalidad por Mes',
+            11 => 'Informativo Mes',
+            12 => 'Informativo Acumulado',
+            13 => 'Incremento y Decremento de la Incidencia',
+            14 => 'Incidencia Delictiva por Mes',
+            15 => 'Total de Delitos por Modalidad',
+            16 => 'Trata de Personas',
+            17 => 'Feminicidios',
+            18 => 'Gráficas Alto Impacto'
+        ];
+        $rangos = session('rangos', []);
+
+        return view('reportes', compact(['reportes', 'rangos']));
+    }
+
+    public function procesarReporte (Request $request) {
+        $reporte = $request->validate([
+            'reporte' => 'required'
+        ]);
+        $rangos = session('rangos', []);
+
+        $redirecciones = [
+            1 => 'redireccion 1',
+            2 => 'redireccion 2',
+            3 => 'redireccion 3',
+            4 => 'redireccion 4',
+            5 => 'redireccion 5',
+            6 => 'redireccion 6',
+            7 => 'redireccion 7',
+            8 => 'redireccion 8',
+            9 => 'redireccion 9',
+            10 => 'redireccion 10',
+            11 => 'redireccion 11',
+            12 => 'redireccion 12',
+            13 => 'redireccion 13',
+            14 => 'redireccion 14',
+            15 => 'redireccion 15',
+            16 => 'redireccion 16',
+            17 => 'redireccion 17',
+            18 => 'redireccion 18'
+        ];
+
+        if (array_key_exists($reporte['reporte'], $redirecciones)) {
+            return $redirecciones[$reporte['reporte']];
+        } else {
+            return 'No está';
+        }
+        
+        // return response()->json([
+        //     'mensaje' => 'Datos recibidos con éxito.',
+        //     'rangos' => $rangos,
+        //     'reporte' => $reporte,
+        // ]);    
+    }
+
+    public function exportExcel (Request $request) {
+        return Excel::download(new ReporteExport($request->all(), 'reporte.xlsx'));
+    }
+}
